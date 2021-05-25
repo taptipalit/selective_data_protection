f87415c9373cb7177fd8ffc9793ef19bd7a81908
Tapti Palit <tpalit@cs.stonybrook.edu>
2021-05-24 23:55:25 -0400

    cleanup

--- test/Datarand/run.sh
+++ test/Datarand/run.sh
@@ -39,20 +39,9 @@ then
     exit 1
 fi
 
-#wpa -nander -keep-self-cycle=all -dump-consG -dump-pag -print-all-pts $file.bc
-#wpa -ander -keep-self-cycle=all -dump-consG -dump-pag -print-all-pts $file.bc
-
-#$LLVMROOT/opt -wpa -print-all-pts -dump-pag -dump-consG $file.ll  -o $fileinst.bc 
 $LLVMROOT/opt -encryption  $file.bc -o $fileinst.bc 
-# -fullanders -dump-pag -print-all-pts -dump-callgraph -dump-consG 
-#$LLVMROOT/opt -test-transform $file.bc  -o $fileinst.bc
 $LLVMROOT/llvm-dis $file.bc -o $file.ll
 $LLVMROOT/llvm-dis $fileinst.bc -o $fileinst.ll
-#exit 0
-#dot -Tpng pag_final.dot -o $file"_pag_final.png"
-#dot -Tpng pag_initial.dot -o $file"_pag_initial.png"
-#dot -Tpng consCG_final.dot -o $file"_consg_full_final.png"
-#dot -Tpng consCG_selective_final.dot -o $file"_consg_selective_final.png"
 
 $LLVMROOT/llc -O0 -filetype=obj $fileinst.bc -o $fileinst.o
 if [ $? -ne 0 ]
